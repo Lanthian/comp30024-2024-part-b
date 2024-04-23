e5fd5c88efd97b8dc15b5b390491c216e49255ce
lanthian <lanthian@student.unimelb.edu.au>
2024-04-23 10:14:18 +0000

    Applicable Part A code imported. Imports are currently broken.

---
+++ agent/prioritydict.py
@@ -0,0 +1,52 @@
+"""prioritydict.py: Implements a priority dictionary data structure that stores
+equal priority items in LIFO queues."""
+
+__author__ = "Liam Anthian"
+__credits__ = ["Liam Anthian", "Anthony Hill"] 
+
+# COMP30024 Artificial Intelligence, Semester 1 2024
+# Project Part B: Single Player Tetress
+
+from queue import LifoQueue
+
+
+class PriorityDict:
+    """
+    A data structure that acts like a priority queue where equal priority items 
+    have LIFO behaviour. Exists in dictionary form to allow O(1) insertion of
+    items as opposed to the O(log(m)) insertion of Queue.PriorityQueue.
+    """
+    # Class fields - initialised on creation
+    items: dict[int, LifoQueue]={}
+    size: int=0         # tracks size as operations performed
+
+    def put(self, priority, item):
+        """Adds an item of priority `priority` to a PriorityDict."""
+        if priority not in self.items.keys():
+            # No items of priority yet, create listing
+            self.items[priority] = LifoQueue()
+        self.items[priority].put(item)
+        self.size += 1
+        # PriorityQueue.put(self.items[priority], item)
+
+    def get(self):
+        """Returns the smallest item in a PriorityDict."""
+        k = list(self.items.keys())
+        if len(k) == 0:
+            # No items as of current
+            return None
+        smallest = min(k)
+        
+        # Otherwise, return smallest item
+        t = self.items[smallest].get()
+        if self.items[smallest].empty():
+            # Delete key in dict if last of given priority
+            del self.items[smallest]
+        
+        self.size -= 1
+        return t
+    
+    def empty(self) -> bool:
+        """Returns true if PriorityDict has no items, false otherwise."""
+        return len(list(self.items.keys())) == 0
+    
--- agent/program.py
+++ agent/program.py
@@ -1,16 +1,30 @@
 # COMP30024 Artificial Intelligence, Semester 1 2024
 # Project Part B: Game Playing Agent
 
-from referee.game import PlayerColor, Action, PlaceAction, Coord
+from .prioritydict import *
+from .tetrominoes import *
+from referee.game import PlayerColor, Action, PlaceAction, Coord, BOARD_N
+# from agent import tetrominoes_plus
+# from agent import PriorityDict
+# from agent import PriorityDict
+# from agent import tetrominoes_plus
 
-# python -m referee agent agent
-BOARD_SIZE = 11  # size of board
+
+""" class Gamestate:
+    # board representation
+    # current player
+    # heuristic value
+    # # below count done as to minimise recalculation of dictionary elements
+    # count of red
+    # count of blue """
+
+# python -m referee agent agent                 todo / temp
 class Agent:
     """
     This class is the "entry point" for your agent, providing an interface to
     respond to various Tetress game events.
     """
-    
+    first_move: bool=True
 
     def __init__(self, color: PlayerColor, **referee: dict):
         """
@@ -26,9 +40,10 @@ class Agent:
         """
         Initialize or reset the game board to an empty state.
         """
-        for r in range(BOARD_SIZE):
-            for c in range(BOARD_SIZE):
-                self.game_board[(r, c)] = None
+        for r in range(BOARD_N):
+            for c in range(BOARD_N):
+                self.game_board[Coord(r,c)] = None
+                # sparse game board might be better here, but we'll see
 
 
     def action(self, **referee: dict) -> Action:
@@ -43,25 +58,37 @@ class Agent:
         # the agent is playing as BLUE or RED. Obviously this won't work beyond
         # the initial moves of the game, so you should use some game playing
         # technique(s) to determine the best action to take.
-        match self.color:
-            case PlayerColor.RED:
-                print("Testing: RED is playing a PLACE action")
-                #nextMove = self.getNextMove(self.color)
-                return PlaceAction(
-                    Coord(5, 4), 
-                    Coord(3, 4), 
-                    Coord(4, 3), 
-                    Coord(4, 4)
-                )
-            case PlayerColor.BLUE:
-                print("Testing: BLUE is playing a PLACE action")
-                #nextMove = self.getNextMove(self.color)
-                return PlaceAction(
-                    Coord(2, 3), 
-                    Coord(2, 4), 
-                    Coord(2, 5), 
-                    Coord(2, 6)
-                )
+        
+        if self.first_move:
+            self.first_move=False
+
+            match self.color:
+                case PlayerColor.RED:
+                    print("Testing: RED is playing a PLACE action")
+                    #nextMove = self.getNextMove(self.color)
+                    return PlaceAction(
+                        Coord(5, 4), 
+                        Coord(3, 4), 
+                        Coord(4, 3), 
+                        Coord(4, 4)
+                    )
+                case PlayerColor.BLUE:
+                    print("Testing: BLUE is playing a PLACE action")
+                    #nextMove = self.getNextMove(self.color)
+                    return PlaceAction(
+                        Coord(2, 3), 
+                        Coord(2, 4), 
+                        Coord(2, 5), 
+                        Coord(2, 6)
+                    )
+        
+        else:
+            pd = PriorityDict()
+            for move in possible_moves(self.game_board, self.color):
+                pd.put(0, move) # insert all moves as equal cost for now...
+
+            return pd.get()
+
 
     def update(self, color: PlayerColor, action: Action, **referee: dict):
         """
@@ -73,15 +100,127 @@ class Agent:
         """
 
 
-        # There is only one action type, PlaceAction
-        for coord in action.coords:
-                self.game_board[(coord.r, coord.c)] = color
-
+        # There is only one action type, PlaceAction. 
+        # Clear filled lines as necessary.
+        make_place(self.game_board, action, color)
+        
         c1, c2, c3, c4 = action.coords
 
         # Here we are just printing out the PlaceAction coordinates for
         # demonstration purposes. You should replace this with your own logic
         # to update your agent's internal game state representation.
         print(f"Testing: {color} played PLACE action: {c1}, {c2}, {c3}, {c4}")
-
         
+
+def possible_moves(board: dict[Coord, PlayerColor], 
+                   player: PlayerColor) -> list[PlaceAction]:
+    """
+    Takes a game `board` and a `player` defined by their PlayerColor and returns 
+    all possible next moves for said player in the form of a list of 
+    PlaceActions.
+    """
+    moves = set()
+    for (coord, color) in board.items():
+        if color == player:
+            # duplicate moves generated and ignored here (possible improvement)
+            moves.update(tetrominoes_plus(coord, set(board.keys())))
+
+    return list(moves)
+
+
+def free_cells(
+    board: dict[Coord, PlayerColor], 
+    target: Coord,
+    axis: str
+) -> int | None:
+    """Takes a dictionary of board tokens `board`, a Coord `target`, and an 
+    `axis` flag to determine which board axis is counted over. Utilises the fact 
+    that the board is a sparse representation of tokens present by checking if 
+    dict contains each axis coordinate.
+
+    Returns:
+        Either the count of free cells in target axis, or None if invalid `axis`
+        flag supplied.
+    """    
+    # Find which axis is iterated over
+    match axis:
+        case "x" | "r" | "row":
+            axis_iterator = lambda t: Coord(target.r, t)
+        case "y" | "c" | "col":
+            axis_iterator = lambda t: Coord(t, target.c)
+        case _:
+            print("ERROR free_cells: invalid axis specified.")
+            return None
+
+    free = BOARD_N
+    # Subtract occupied cells to find free count
+    for i in range(BOARD_N):
+        if axis_iterator(i) in board:
+            free -= 1
+
+    return free
+
+
+def clear_axes(
+    board: dict[Coord, PlayerColor],
+    row_range: list[int]=range(BOARD_N),
+    col_range: list[int]=range(BOARD_N)
+) -> None:
+    """
+    Acts in place - clears filled rows and columns on a game `board`. Only 
+    checks for clearable axes in the row and column ranges supplied, to make 
+    targetted clearing more efficient. Checks all rows and columns by default. 
+    Returns nothing (None).
+    """
+    IRRELEVANT = 0
+
+    # Find all cells that exist in checked filled rows/cols
+    to_clear = set()
+    for r in row_range:
+        if free_cells(board, Coord(r,IRRELEVANT), "r") == 0:
+            [to_clear.add(Coord(r,i)) for i in range(BOARD_N)]
+    for c in col_range:
+        if free_cells(board, Coord(IRRELEVANT,c), "c") == 0:
+            [to_clear.add(Coord(i,c)) for i in range(BOARD_N)]
+
+    # Drop these cells from board
+    for tile in to_clear:
+        board.pop(tile, 0)
+
+    return None
+    
+
+def make_place(
+    board: dict[Coord, PlayerColor], 
+    place: PlaceAction, 
+    color: PlayerColor,
+) -> dict[Coord, PlayerColor]:
+    """
+    Assumes the place actions have been validated first, otherwise it can write
+    over the top of existing cells. Places tetrominoes on a board, clearing rows
+    / cols if filled. Acts in place - use with .copy() to generate new boards.
+
+    Parameters:
+        `board`: a dictionary representing the initial board state, mapping
+            coordinates to "player colours". The keys are `Coord` instances,
+            and the values are `PlayerColor` instances.  
+        `place`: a `PlaceAction`instance of four coordinates of a tetromino
+            piece to place onto the board.
+        `color`: the core.py `PlayerColor` of the tetromino piece being played
+    
+    Returns:
+        An altered board containing the changes of adding given tetromino piece
+        and clearing now full axes.
+    """
+    placed_r = set()
+    placed_c = set()
+
+    for coord in place.coords:
+        # Add coordinate axes to tracking sets then place token
+        placed_r.add(coord.r)
+        placed_c.add(coord.c)
+        board[coord] = color
+
+    # If necessary, clear now full rows and columns
+    clear_axes(board, list(placed_r), list(placed_c))
+    return board
